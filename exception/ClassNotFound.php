09c2ea01cffefbfbe7825589173fb7e5210d5682
Thomas Hunter <tlhunter@gmail.com>
2011-09-27 19:51:07 -0400

    Moving files from last repo to this submodule

---
+++ exception/ClassNotFound.php
@@ -0,0 +1,6 @@
+<?php
+namespace Sleek;
+
+class Exception_ClassNotFound extends \Exception {
+
+}
---
+++ model/Base.php
@@ -0,0 +1,9 @@
+<?php
+namespace Sleek;
+
+/**
+ * All models should inherit from this (currently empty) class
+ */
+abstract class Model_Base {
+
+}
