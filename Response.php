fa92d7521d4ab7a5a902adebe2e114283b97b0da
Thomas Hunter <tlhunter@gmail.com>
2013-05-18 20:55:57 -0400

    handle more HTTP status codes

--- Response.php
+++ Response.php
@@ -5,11 +5,57 @@ class Response {
     /**
      * @var Response The singleton instance of our response class
      */
-    static private $_instance   = NULL;
+    static private $_instance = NULL;
 
-    private function __construct() {
+    /**
+     * @var array A listing of HTTP status codes
+     * @author http://coreymaynard.com/blog/creating-a-restful-api-with-php/
+     */
+    static public $status = array(
+        100 => 'Continue',
+        101 => 'Switching Protocols',
+        200 => 'OK',
+        201 => 'Created',
+        202 => 'Accepted',
+        203 => 'Non-Authoritative Information',
+        204 => 'No Content',
+        205 => 'Reset Content',
+        206 => 'Partial Content',
+        300 => 'Multiple Choices',
+        301 => 'Moved Permanently',
+        302 => 'Found',
+        303 => 'See Other',
+        304 => 'Not Modified',
+        305 => 'Use Proxy',
+        306 => '(Unused)',
+        307 => 'Temporary Redirect',
+        400 => 'Bad Request',
+        401 => 'Unauthorized',
+        402 => 'Payment Required',
+        403 => 'Forbidden',
+        404 => 'Not Found',
+        405 => 'Method Not Allowed',
+        406 => 'Not Acceptable',
+        407 => 'Proxy Authentication Required',
+        408 => 'Request Timeout',
+        409 => 'Conflict',
+        410 => 'Gone',
+        411 => 'Length Required',
+        412 => 'Precondition Failed',
+        413 => 'Request Entity Too Large',
+        414 => 'Request-URI Too Long',
+        415 => 'Unsupported Media Type',
+        416 => 'Requested Range Not Satisfiable',
+        417 => 'Expectation Failed',
+        500 => 'Internal Server Error',
+        501 => 'Not Implemented',
+        502 => 'Bad Gateway',
+        503 => 'Service Unavailable',
+        504 => 'Gateway Timeout',
+        505 => 'HTTP Version Not Supported'
+    );
 
-    }
+    private function __construct() { }
 
     /**
      * Prevents the class from being cloned
@@ -25,6 +71,7 @@ class Response {
         if (!self::$_instance) {
             self::$_instance = new Response();
         }
+
         return self::$_instance;
     }
 
@@ -39,10 +86,13 @@ class Response {
         if (headers_sent()) {
             return FALSE;
         }
+
         if ($permanent) {
             header("HTTP/1.1 301 Moved Permanently");
         }
+
         header("Location: $url");
+
         return TRUE;
     }
 
@@ -57,11 +107,13 @@ class Response {
         if (headers_sent()) {
             return FALSE;
         }
+
         $header = $name;
         if ($value) {
             $header .= ": $value";
         }
         header($header);
+
         return TRUE;
     }
 
@@ -73,34 +125,12 @@ class Response {
      * @return bool
      */
     public static function status($code) {
-        if (headers_sent()) {
+        if (headers_sent() || !isset(self::$status[$code])) {
             return FALSE;
         }
-        
-        $text = '';
-        switch($code) {
-            case '200':
-                $text = "OK";
-                break;
-            case '400':
-                $text = "Bad Request";
-                break;
-            case '401':
-                $text = "Unauthorized";
-                break;
-            case '403':
-                $text = "Forbidden";
-                break;
-            case '404':
-                $text = "Not Found";
-                break;
-            case '500':
-                $text = "Internal Server Error";
-                break;
-            default:
-                return FALSE;
-        }
-        header('HTTP/1.1 ' . $code . ' ' . $text);
+
+        header("HTTP/1.1 $code " . self::$status[$code]);
+
         return TRUE;
     }
 
@@ -131,6 +161,7 @@ class Response {
         if (headers_sent()) {
             return FALSE;
         }
+
         return setcookie($name, $value, $expire, $path, $domain, $secure, $httponly);
     }
 }
